f5ec9b0c98addbd4ac38b9938d93ead17c68662b
loretari <loretari@gmail.com>
2023-08-01 16:34:13 +0300

    init: bootstrap the Host pages

--- index.jsx
+++ index.jsx
@@ -6,6 +6,11 @@ import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 import Layout from "./components/Layout"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+
+
 
 import "./server"
 
@@ -19,6 +24,9 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path= "/vans" element={<Vans/>}  />
               <Route path= "/vans/:id" element={<VanDetail />}/>
+              <Route path= "/host" element={<Dashboard />} />
+              <Route path= "/host/income" element={<Income />} />
+              <Route path= "/host/reviews" element={<Reviews />} />
           </Route>
       </Routes>
 
