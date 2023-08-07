0cbb4044190b4a7a241e426ba53c3d860c821342
loretari <loretari@gmail.com>
2023-08-07 08:23:25 +0300

    init: nested the host routes

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom"
 
 export default function Dashboard() {
     return (
         <div>
             <h1>Host Dashboard here</h1>
+            <Outlet/>
         </div>
     )
 
