8787254670e7b6d1dadaf3bc0a8f1e48f149b41c
loretari <loretari@gmail.com>
2023-08-07 13:04:01 +0300

    fix bug

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Outlet, NavLink } from "react-router-dom"
 
 export default function HostLayout () {
-    const activeStyle = {
-        fontWeight: "bold",
-         textDecoration: "underline",
-          color: "#161616"
-    }
+
 
 
 
@@ -17,18 +13,35 @@ export default function HostLayout () {
     }
     return (
         <div>
-
-                >
-                    Income
-                </NavLink>
-                <NavLink
-
-                >
-                    Reviews
-                </NavLink>
-            </nav>
-            <Outlet />
+            <nav className= "host-nav">
+            <NavLink
+                to= "/host"
+                end
+                style = {({isActive}) => isActive ? activeStyle : null}
+            >
+                Dashboard</NavLink>
+            <NavLink
+                to= "/host/income"
+                style = {({isActive}) => isActive ? activeStyle : null}
+            >
+                Income
+            </NavLink>
+            <NavLink
+                to= "/host/vans"
+                style = {({isActive}) => isActive ? activeStyle : null}
+            >
+                Vans
+            </NavLink>
+            <NavLink
+                to= "/host/reviews"
+                style = {({isActive}) => isActive ? activeStyle : null}
+            >
+                Reviews
+            </NavLink>
+        </nav>
+        <Outlet />
         </div>
+
 
     )
 }
