cd4f83f36fcf398a43254b6b2cfc0beb8cf5ba94
loretari <loretari@gmail.com>
2023-08-07 12:55:54 +0300

    init: add host vans routes

---
+++ pages/Host/HostVanDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function HostVanDetail() {
+
+    return (
+        <div>
+            <h1>Host Van Detail Pages</h1>
+        </div>
+    )
+}
---
+++ pages/Host/HostVans.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HostVans() {
+
+    return (
+        <div>
+            <h1>Your listed vans</h1>
+        </div>
+    )
+}
