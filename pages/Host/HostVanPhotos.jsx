6f2cfbb9316bc277a9d8810d8917b145aa443547
loretari <loretari@gmail.com>
2023-08-08 10:25:12 +0300

    init: add Outlet context

--- pages/Host/HostVanPhotos.jsx
+++ pages/Host/HostVanPhotos.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
+
 
 export default function HostVanPhotos() {
+ const { currentVan } = useOutletContext()
 
     return(
-        <div>
-            <h2>Photos</h2>
-        </div>
+       <img src={currentVan.imageUrl} className = "host-van-detail-image"/>
     )
 }
