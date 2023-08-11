a124d0cfce3a20d1b0022616ab8155d39f76bb92
loretari <loretari@gmail.com>
2023-08-11 09:55:46 +0300

    initial: setting up for authentication

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <h1>&#169; 2022 #VANLIFE</h1>
+            <h1>&#169; 2023 #VANLIFE</h1>
         </footer>
     )
 }
