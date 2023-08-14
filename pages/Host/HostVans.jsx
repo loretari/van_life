8750da74f427903d1010f960d61e27a9c7f1ea97
loretari <loretari@gmail.com>
2023-08-14 12:52:58 +0300

    initial: add redirectTo and request

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -4,8 +4,8 @@ import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
 
 
-export async function loader() {
-    await requireAuth()
+export async function loader({ request }) {
+    await requireAuth(request)
     return getHostVans()
 }
 
