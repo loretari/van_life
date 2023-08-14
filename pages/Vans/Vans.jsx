7d1aa712a86fce59878b3c9b12ac86c9f18347b2
loretari <loretari@gmail.com>
2023-08-14 15:24:22 +0300

    initial: add defer to getVans

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import { Link, useSearchParams, useLoaderData, defer } from "react-router-dom";
 import { getVans } from "../../api";
 
 
-export function loader() {
-    return getVans()
+export async function loader() {
+    return defer({ vans: getVans() })
 }
+
+
 
 export default function Vans() {
 
