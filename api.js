c3dbe598547f07aa1d39feb5e672bc991f68eaef
loretari <loretari@gmail.com>
2023-08-15 15:25:25 +0300

    initial: add Firebase and Firestore

--- api.js
+++ api.js
@@ -1,3 +1,24 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB7vE-_CaCC6siAbsjhnIABuKHnc2_pL3k",
+    authDomain: "vanlife-1eb22.firebaseapp.com",
+    projectId: "vanlife-1eb22",
+    storageBucket: "vanlife-1eb22.appspot.com",
+    messagingSenderId: "527651427894",
+    appId: "1:527651427894:web:35dc342339a84c53a22a97"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+// Refactoring the fetching functions
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
